912edd1f971f6b0585de8cb7e64695cdb2c890ce
Rocky  Luo <rocky.luo@rutgers.edu>
2016-11-14 22:11:44 -0500

    Update db_accessor.py

--- crawler_server/crawl_job/db_accessor.py
+++ crawler_server/crawl_job/db_accessor.py
@@ -3,11 +3,11 @@ import psycopg2
 # INSERT INTO pokemon_map (encounter_id , expire , pokemon_id , latitude , longitude ) VALUES (1,1,1,1,1) ON CONFLICT (encounter_id) DO NOTHING;
 def add_pokemon_to_db(encounter_id , expire , pokemon_id , latitude , longitude):
     # 1. Open connection
-    conn = psycopg2.connect(host = "week4demo-before.cozbedhsxp0j.us-west-2.rds.amazonaws.com",
+    conn = psycopg2.connect(host = "week2demo3.chvipb2vfhay.us-west-2.rds.amazonaws.com",
                             port = 5432,
-                            user = "week4demo_before",
-                            password = "week4demo_before",
-                            database = "week4demo_before")
+                            user = "week2demo3",
+                            password = "week2demo3",
+                            database = "week2demo3")
 
     # 2. Execute SQL
     with conn.cursor() as cur:
